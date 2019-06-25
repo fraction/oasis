5ff11a2a4e720b2da385e05bf06faac03748c44f
Christian Bundy <christianbundy@fraction.io>
2019-06-24 17:07:18 -0700

    Refactor to reduce code duplication

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const path = require('path')
 const pull = require('pull-stream')
 const router = require('koa-router')()
 const views = require('koa-views')
+const ssbRef = require('ssb-ref')
+
 const cooler = require('./lib/cooler')
-const md = require('ssb-markdown')
-const lodash = require('lodash')
-const prettyMs = require('pretty-ms')
+const renderMsg = require('./lib/render-msg')
 
 const app = module.exports = new Koa()
 
@@ -24,62 +24,22 @@ router
 app.use(router.routes())
 
 async function message (ctx) {
-  var ssb = await cooler.connect()
-  var msg = await cooler.get(ssb.get, {
+  const ssb = await cooler.connect()
+  const rawMsg = await cooler.get(ssb.get, {
     id: ctx.params.id,
     meta: true,
     private: true
   })
 
-  async function fixUp (msg) {
-    lodash.set(msg, 'value.meta.md.block', () =>
-      md.block(msg.value.content.text)
-    )
-
-    const name = await cooler.get(
-      ssb.about.socialValue, { key: 'name',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarMsg = await cooler.get(
-      ssb.about.socialValue, { key: 'image',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarId = avatarMsg != null && typeof avatarMsg.link === 'string'
-      ? avatarMsg.link
-      : avatarMsg
-
-    const avatarUrl = `http://localhost:8989/blobs/get/${avatarId}`
-
-    const ts = new Date(msg.value.timestamp)
-    lodash.set(msg, 'value.meta.timestamp.received.iso8601', ts.toISOString())
-    lodash.set(msg, 'value.meta.timestamp.received.since', prettyMs(Date.now() - ts, { compact: true }))
-    lodash.set(msg, 'value.meta.author.name', name)
-    lodash.set(msg, 'value.meta.author.avatar', {
-      id: avatarId,
-      url: avatarUrl
-    })
-
-    return msg
-  }
-
-  var fixedMsg = await fixUp(msg)
-  await ctx.render('single-message', { msg: fixedMsg })
+  const msg = await renderMsg(ssb)(rawMsg)
+  await ctx.render('single-message', { msg })
 }
 
 async function author (ctx) {
+  if (ssbRef.isFeed(ctx.params.id) === false) {
+    throw new Error(`not a feed: ${ctx.params.id}`)
+  }
   var ssb = await cooler.connect()
-  var whoami = await cooler.get(ssb.whoami)
-
-  const userName = await cooler.get(
-    ssb.about.socialValue, {
-      key: 'name',
-      dest: whoami.id
-    }
-  )
 
   var msgSource = await cooler.read(
     ssb.createUserStream, {
@@ -104,53 +64,13 @@ async function author (ctx) {
     )
   })
 
-  const msgs = await Promise.all(rawMsgs.map(async (msg) => {
-    lodash.set(msg, 'value.meta.md.block', () =>
-      md.block(msg.value.content.text)
-    )
+  const msgs = await Promise.all(rawMsgs.map(renderMsg(ssb)))
 
-    const name = await cooler.get(
-      ssb.about.socialValue, { key: 'name',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarMsg = await cooler.get(
-      ssb.about.socialValue, { key: 'image',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarId = avatarMsg != null && typeof avatarMsg.link === 'string'
-      ? avatarMsg.link
-      : avatarMsg
-
-    const avatarUrl = `http://localhost:8989/blobs/get/${avatarId}`
-
-    const ts = new Date(msg.value.timestamp)
-    lodash.set(msg, 'value.meta.timestamp.received.iso8601', ts.toISOString())
-    lodash.set(msg, 'value.meta.timestamp.received.since', prettyMs(Date.now() - ts, { compact: true }))
-    lodash.set(msg, 'value.meta.author.name', name)
-    lodash.set(msg, 'value.meta.author.avatar', {
-      id: avatarId,
-      url: avatarUrl
-    })
-
-    return msg
-  }))
-
-  await ctx.render('home', { whoami, msgs, userName })
+  await ctx.render('home', { msgs })
 }
+
 async function home (ctx) {
   var ssb = await cooler.connect()
-  var whoami = await cooler.get(ssb.whoami)
-
-  const userName = await cooler.get(
-    ssb.about.socialValue, {
-      key: 'name',
-      dest: whoami.id
-    }
-  )
 
   var msgSource = await cooler.read(
     ssb.messagesByType, {
@@ -171,42 +91,9 @@ async function home (ctx) {
     )
   })
 
-  const msgs = await Promise.all(rawMsgs.map(async (msg) => {
-    lodash.set(msg, 'value.meta.md.block', () =>
-      md.block(msg.value.content.text)
-    )
+  const msgs = await Promise.all(rawMsgs.map(renderMsg(ssb)))
 
-    const name = await cooler.get(
-      ssb.about.socialValue, { key: 'name',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarMsg = await cooler.get(
-      ssb.about.socialValue, { key: 'image',
-        dest: msg.value.author
-      }
-    )
-
-    const avatarId = avatarMsg != null && typeof avatarMsg.link === 'string'
-      ? avatarMsg.link
-      : avatarMsg
-
-    const avatarUrl = `http://localhost:8989/blobs/get/${avatarId}`
-
-    const ts = new Date(msg.value.timestamp)
-    lodash.set(msg, 'value.meta.timestamp.received.iso8601', ts.toISOString())
-    lodash.set(msg, 'value.meta.timestamp.received.since', prettyMs(Date.now() - ts, { compact: true }))
-    lodash.set(msg, 'value.meta.author.name', name)
-    lodash.set(msg, 'value.meta.author.avatar', {
-      id: avatarId,
-      url: avatarUrl
-    })
-
-    return msg
-  }))
-
-  await ctx.render('home', { whoami, msgs, userName })
+  await ctx.render('home', { msgs })
 }
 
 if (!module.parent) {
