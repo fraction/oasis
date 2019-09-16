4a9361a8b63fc1e132c5e06105bc0015a0eecc68
Christian Bundy <christianbundy@fraction.io>
2019-09-16 13:27:55 -0700

    Refactor SSB distro into @fraction/flotilla module

--- src/pages/models/lib/cooler.js
+++ src/pages/models/lib/cooler.js
@@ -1,34 +1,11 @@
 'use strict'
 
-const ssbClient = require('ssb-client')
-const secretStack = require('secret-stack')
-const ssbConfig = require('ssb-config')
 const debug = require('debug')('oasis')
+const flotilla = require('@fraction/flotilla')
+const ssbClient = require('ssb-client')
+const ssbConfig = require('ssb-config')
 
-const server = secretStack()
-
-server
-  .use(require('ssb-db'))
-  .use(require('ssb-replicate'))
-
-  .use(require('ssb-about'))
-  .use(require('ssb-backlinks'))
-  .use(require('ssb-blobs'))
-  .use(require('ssb-ebt'))
-  .use(require('ssb-friends'))
-  .use(require('ssb-gossip'))
-  .use(require('ssb-invite'))
-  .use(require('ssb-links'))
-  .use(require('ssb-local'))
-  .use(require('ssb-logging'))
-  .use(require('ssb-master'))
-  .use(require('ssb-no-auth'))
-  .use(require('ssb-onion'))
-  .use(require('ssb-ooo'))
-  .use(require('ssb-plugins'))
-  .use(require('ssb-query'))
-  .use(require('ssb-unix-socket'))
-  .use(require('ssb-ws'))
+const server = flotilla()
 
 const rawConnect = () => new Promise((resolve, reject) => {
   ssbClient((err, api) => {
