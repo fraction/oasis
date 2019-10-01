fddccfc40f673d12bd21f6e2defdef05983f566d
Christian Bundy <christianbundy@fraction.io>
2019-09-30 18:20:28 -0700

    Fix incorrect default theme in text

--- src/pages/views/status.js
+++ src/pages/views/status.js
@@ -161,7 +161,7 @@ module.exports = ({ status, theme }) => {
   return template(
     section({ class: 'message' },
       h1('Theme'),
-      p('Choose from any theme you\'d like. The default theme is Tomorrow. Other favorites are Ashes, Chalk, Default, Eighties, Google, Harmonic16, IR Black, Monokai, Rebecca, Solarized, Summerfruit, and Tomorrow.'),
+      p('Choose from any theme you\'d like. The default theme is Unikitty Light. Other favorites are Ashes, Chalk, Default, Eighties, Google, Harmonic16, IR Black, Monokai, Rebecca, Solarized, Summerfruit, and Tomorrow.'),
       form({ action: '/theme.css', method: 'post' },
         select({ name: 'theme' }, ...themeElements),
         button({ type: 'submit' }, 'set theme')),
