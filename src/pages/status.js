ea73181396f47df1881e29e0ebd9035e1a5bf6e3
Christian Bundy <christianbundy@fraction.io>
2019-10-15 21:00:03 -0700

    Change to use new API from `@fraction/base16-css`

--- src/pages/status.js
+++ src/pages/status.js
@@ -2,26 +2,11 @@
 
 const meta = require('./models/meta')
 const statusView = require('./views/status')
-const fs = require('fs').promises
-const path = require('path')
 
 module.exports = async function statusPage ({ theme }) {
   const status = await meta.status()
 
-  // TODO: make `main` property in @fraction/base16-css
-  const moduleName = '@fraction/base16-css'
-  const randomTheme = 'monokai'
-  const requirePath = `${moduleName}/src/base16-${randomTheme}.css`
-
-  const filePath = require.resolve(requirePath)
-  const dirPath = path.dirname(filePath)
-  const fileNames = await fs.readdir(dirPath)
-
-  const themeNames = fileNames.map((fileName) => {
-    const withoutPrefix = fileName.replace('base16-', '')
-    const withoutSuffix = withoutPrefix.replace('.css', '')
-    return withoutSuffix
-  })
+  const { themeNames } = require('@fraction/base16-css')
 
   return statusView({ status, theme, themeNames })
 }
