3c78a6ac6c6188445ab286976f666222b2641330
Christian Bundy <christianbundy@fraction.io>
2019-12-12 15:36:32 -0800

    Fix bug with uppercase in search query

--- src/pages/search.js
+++ src/pages/search.js
@@ -4,6 +4,11 @@ const post = require('./models/post')
 const searchView = require('./views/search')
 
 module.exports = async function searchPage ({ query }) {
+  if (typeof query === 'string') {
+    // https://github.com/ssbc/ssb-search/issues/7
+    query = query.toLowerCase()
+  }
+
   const messages = await post.search({ query })
 
   return searchView({ messages, query })
