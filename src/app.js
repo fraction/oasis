2ec97940dfdf117b575d273d7dc9cd3fa028a37a
Christian Bundy <christianbundy@fraction.io>
2019-12-15 14:08:38 -0800

    Replace faux-shadow with thin border, change default theme

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ const markdown = require('./pages/markdown')
 const inboxPage = require('./pages/inbox')
 const searchPage = require('./pages/search')
 
-const defaultTheme = 'tomorrow'
+const defaultTheme = 'atelier-sulphurPool-light'.toLowerCase()
 
 module.exports = (config) => {
   const assets = new Koa()
