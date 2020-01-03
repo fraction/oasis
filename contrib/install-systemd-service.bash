7af34f6b4e4e98ede4774c047c1df99ff134b191
Jonathan Dahan <hi@jonathan.is>
2020-01-03 15:09:45 -0500

    copy script no matter what dir the script is in

--- contrib/install-systemd-service.bash
+++ contrib/install-systemd-service.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=`dirname "$0"`
 test "$XDG_CONFIG_HOME" || XDG_CONFIG_HOME=$HOME/.config
 test "$SYSTEMD_USER_HOME" || SYSTEMD_USER_HOME="$XDG_CONFIG_HOME"/systemd/user
 mkdir -p "$SYSTEMD_USER_HOME"
-test -f "$SYSTEMD_USER_HOME"/oasis.service || cp oasis.service "$SYSTEMD_USER_HOME"/
+test -f "$SYSTEMD_USER_HOME"/oasis.service || cp "$SCRIPT_DIR"/oasis.service "$SYSTEMD_USER_HOME"/
 systemctl --user daemon-reload
 
 printf "oasis service has been installed to %s\n\n" "$SYSTEMD_USER_HOME"
