3a486c851fba35c8f9906fea881eff72a4d86156
Christian Bundy <christianbundy@fraction.io>
2020-02-25 16:20:33 -0800

    Replace shell script with C program for max compat
    Problem: The shell script didn't work on other platforms and the
    AppImage hack ended up confusing people who tried to install it with
    AppImageLauncher.
    
    Solution: This feels absolutely ridiculous, but I've written the worst C
    program in the world which does the equivalent of `exec ./node src "$@"`
    and immediately gets out of our way. This ends up being an executable
    that both Nautilus and Dolphin (GNOME and KDE file managers) let us
    double-click, and should also work on macOS. It might even work on
    Windows if we rename it as an `.exe`, but I'm not sure and *definitely*
    haven't tested that.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+
 BASEDIR="$(dirname "$0")"
 
 cd "$BASEDIR/.."
@@ -13,6 +14,13 @@ TARGET_ARCH="$2"
 echo "Target version: $TARGET_VERSION"
 echo "Target platform: $TARGET_PLATFORM"
 echo "Target arch: $TARGET_ARCH"
+
+if [ "$TARGET_ARCH" == "x64" ]; then
+	GCC_ARCH="x86-64"
+else
+	echo "Unsupported architecture: $TARGET_ARCH"
+	exit 1
+fi
 
 git clean -fdx
 
@@ -30,33 +38,41 @@ cd ..
 
 # Avoid building anything from source.
 npm ci --only=prod --ignore-scripts --no-audit --no-fund
+# More trouble than it's worth :)
+rm -rf ./node_modules/sharp
 
 BINARY_NAME="oasis"
 
-# Append .appimage for double-click support on Linux
-if [ "$TARGET_PLATFORM" = "linux" ]; then
-  BINARY_NAME="$BINARY_NAME.appimage"
-fi
-
 echo $BINARY_NAME
 
-cat << EOF > "$BINARY_NAME"
-#!/bin/sh
-BASEDIR="\$(cd "\$(dirname "\$0")" && pwd)"
-node="\$BASEDIR/vendor/node-v$TARGET_VERSION-$TARGET_PLATFORM-$TARGET_ARCH/bin/node"
-verify () {
-  node -e "require('\$1')" || echo "Error: \$1 may not work correctly on your platform"
+cat << EOF | gcc -Wall -g -no-pie -o "$BINARY_NAME" -x c -march="$GCC_ARCH" -
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]) {
+	static const char node[] = "./vendor/node-v$TARGET_VERSION-$TARGET_PLATFORM-$TARGET_ARCH/bin/node";
+	static const char src[] = "src";
+
+	char** new_argv = malloc(((argc+1) * sizeof *new_argv) + sizeof src);
+
+	int pad = 0;
+	for(int i = 0; i < argc; i++) {
+		size_t length = strlen(argv[i])+1;
+		new_argv[i+pad] = malloc(length);
+		memcpy(new_argv[i+pad], argv[i], length);
+		if (i == 0) {
+			pad = 1;
+			new_argv[i + pad] = src;
+		}
+	}
+
+	new_argv[argc + 1] = NULL;
+
+	execv(node, new_argv);
+	return 1;
 }
-verify_optional () {
-  node -e "require('\$1')" > /dev/null 2>&1 || rm -rf "node_modules/\$1"
-}
-verify_all () {
-  verify leveldown
-  verify sodium-native
-  verify_optional sharp
-}
-verify_all
-exec "\$node" "src/index.js" "\$@"
+
 EOF
 
 chmod +x "$BINARY_NAME"
